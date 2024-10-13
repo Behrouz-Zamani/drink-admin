6587bd03471beee69705388a33d03f112d8fa084
zamani.behrouz@hotmail.com <zamani.behrouz@hotmail.com>
2024-10-13 19:51:51 +0330

    change main and navBar

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// import NavBar from './layouts/admin/navbar/NavBar.jsx'
+import  NavBar from './layouts/admin/Index.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <NavBar/>
   </StrictMode>,
 )
